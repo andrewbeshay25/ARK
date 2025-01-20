dc30444c74ffecbb1c8348d5fc84c874f41be0bc
Andrew Beshay <102611183+andrewbeshay25@users.noreply.github.com>
2025-01-19 22:54:05 -0500

    Added logout button to home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Home = () => {
-  const firstName = localStorage.getItem("firstName");
+  const { user, logout } = useContext(AuthContext);
 
   return (
     <div>
-      <h1>Hello, {firstName}!</h1>
+      <h1>Hello, {user.firstName}!</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 };
