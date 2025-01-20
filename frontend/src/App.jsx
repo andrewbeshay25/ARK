c95817784054feee6825c4bec21dc5fe7dd947be
Andrew Beshay <102611183+andrewbeshay25@users.noreply.github.com>
2025-01-19 22:53:41 -0500

    Fixed app routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import AuthForm from "./components/AuthForm";
 import Home from "./components/Home";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AuthForm />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AuthForm />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
