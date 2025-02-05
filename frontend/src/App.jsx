3d04326c2b4b504c069718f36e4ed0c46b084142
Andrew Beshay <102611183+andrewbeshay25@users.noreply.github.com>
2025-02-05 12:42:11 -0500

    Added more routes for the app to use

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Profile from "./components/Dashboard/Profile";
 import Settings from "./components/Dashboard/Settings";
 import AuthForm from "./components/AuthForm";
 import PrivateRoute from "./components/PrivateRoute";
+import Home from "./components/pages/Home";
+import CourseDashboard from "./components/Course/CourseDashboard";
 
 const App = () => {
   return (
@@ -13,9 +15,15 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<AuthForm />} />
+          <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
           <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
           <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
+          <Route path="/courses/:courseId/*" element={
+          <PrivateRoute>
+            <CourseDashboard />
+          </PrivateRoute>
+        } />
         </Routes>
       </Router>
     </AuthProvider>
