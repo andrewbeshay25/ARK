5cd08885370af6b4b7a19d40adf8df91a7e8b3fe
andrewbeshay25 <andrewbeshay2525@gmail.com>
2025-06-28 22:03:20 -0500

    Refactor main App component and global styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Profile from "./components/Dashboard/Profile";
+import Profile from "./components/Profile/Profile";
 import Settings from "./components/Dashboard/Settings";
 import AuthForm from "./components/AuthForm";
 import PrivateRoute from "./components/PrivateRoute";
-import Home from "./components/pages/Home";
+import Courses from "./components/pages/Courses";
 import CourseDashboard from "./components/Course/CourseDashboard";
+import AdminDashboard from "./components/Admin/AdminDashboard";
 
 const App = () => {
   return (
@@ -15,15 +16,17 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<AuthForm />} />
-          <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+          <Route path="/home" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/courses" element={<PrivateRoute><Courses /></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
           <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
           <Route path="/courses/:courseId/*" element={
-          <PrivateRoute>
-            <CourseDashboard />
-          </PrivateRoute>
-        } />
+            <PrivateRoute>
+              <CourseDashboard />
+            </PrivateRoute>
+          } />
+          <Route path="/admin" element={<PrivateRoute><AdminDashboard /></PrivateRoute>} />
         </Routes>
       </Router>
     </AuthProvider>
